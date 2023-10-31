1e4488a4c5173eea08219196ca812e100dbbe20a
Rosul Iman <117097805+Iman874@users.noreply.github.com>
2023-10-31 02:28:10 +0000

    Create determinan dan Kofaktor

--- function.py
+++ function.py
@@ -1,3 +1,53 @@
-def fungsi():
-    a = 5
-    return a + 4
+import main
+
+def determinan(persamaanA, persamaanB, persamaanC):
+    # mengambil 3 variabel masing-masing dari persamaanA, persamaanB, persamaanC
+    A1, A2, A3 = persamaanA
+    B1, B2, B3 = persamaanB
+    C1, C2, C3 = persamaanC
+
+    # membuat nilai variabel menjadi positif
+    abs(A1)
+    abs(A3)
+
+    # menyelesaikan determinan dengan persamaanA
+    hasil = A1 * ((B2 * C3)-(B3 * C2)) - A2 * ((B1 * C3)-(B3 * C1)) + A3 * ((B1 * C2)-(B2 * C1))
+
+    return hasil
+
+def kofaktor(persamaanA, persamaanB, persamaanC, status):
+    # mengambil 3 variabel masing-masing dari persamaanA, persamaanB, persamaanC
+    A1, A2, A3 = persamaanA
+    B1, B2, B3 = persamaanB
+    C1, C2, C3 = persamaanC
+
+    # mencari hasil persamaanA
+    hasil_A1 = 1 * ((B2 * C3) - (B3 * C2))
+    hasil_A2 = -1 * ((B1 * C3) - (B3 * C1))
+    hasil_A3 = 1 * ((B1 * C2) - (B2 * C1))
+
+    # mencari hasil persamaanB
+    hasil_B1 = 1 * ((A2 * C3) - (A3 * C2))
+    hasil_B2 = -1 * ((A1 * C3) - (A3 * C1))
+    hasil_B3 = 1 * ((A1 * C2) - (A2 * C1))
+
+    # mencari hasil persamaanC
+    hasil_C1 = 1 * ((A2 * B3) - (A3 * B2))
+    hasil_C2 = -1 * ((A1 * B3) - (A3 * B1))
+    hasil_C3 = 1 * ((A1 * B2) - (A2 * B1))
+
+    # mengambalikan nilai sesuai dengan nilai status
+    if(status == "A"):
+        hasil_tuple = (hasil_A1, hasil_A2, hasil_A3)
+    elif(status == "B"): 
+        hasil_tuple = (hasil_B1, hasil_B2, hasil_B3)
+    elif(status == "C"):
+        hasil_tuple = (hasil_C1, hasil_C2, hasil_C3)
+
+    return hasil_tuple
+
+def garis():
+    print("=========================================")
+
+# menjalankan fungsi pada main
+main.run_main()
--- main.py
+++ main.py
@@ -1,4 +1,35 @@
 import function
 
-hasil = function.fungsi()
-print(hasil)
+# menjalankan program
+run_main()
+
+def run_main():
+    # input internal persamaanA, persamaanB, persamaanC
+    persamaan_A = (1, 2, 1)
+    persamaan_B = (3, -4, -2)
+    persamaan_C = (5, 3, 5)
+
+    # menampilkan matriks yang ada
+    print("Matriks : ")
+    print(persamaan_A)
+    print(persamaan_B)
+    print(persamaan_C)
+    function.garis()
+
+    # mencari determinan kemudian menampilkan hasilnya
+    hasil_determinan = function.determinan(persamaan_A, persamaan_B, persamaan_C)
+
+    # menampilkan hasil determinan
+    print("Hasil determinan adalah", hasil_determinan)
+    function.garis()
+
+    # menampilkan hasil kofaktor
+    print("Hasil Kofaktor")
+    kofakfaktor_A = function.kofaktor(persamaan_A, persamaan_B, persamaan_C, "A")
+    kofakfaktor_B = function.kofaktor(persamaan_A, persamaan_B, persamaan_C, "B")
+    kofakfaktor_C = function.kofaktor(persamaan_A, persamaan_B, persamaan_C, "C")
+
+    # menampilkan hasil kofaktor
+    print(kofakfaktor_A)
+    print(kofakfaktor_B)
+    print(kofakfaktor_C)
